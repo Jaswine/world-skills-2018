bbfa52598de463c720c683c155dc9ac47d0e2545
Vaseles <82625479+Vaseles@users.noreply.github.com>
2023-02-13 13:54:30 +0600

    linies uploading

--- back/line.php
+++ back/line.php
@@ -18,7 +18,7 @@
 
                     <div class="form">
 
-                        <form enctype="multipart/form-data" id="line-form" action="/pruebas/module_e_1/line/create.php" method="post">
+                        <form enctype="multipart/form-data" id="line-form" method="post">
                             <p class="note">Fields with <span class="required">*</span> are required.</p>
 
 
@@ -28,67 +28,67 @@
                             <div class="row">
                                 <label for="Line_start_time_operation" class="required">Start Time Operation <span class="required">*</span></label>        
 
-                                <select name="Line[start_time_operation]" id="start_time_operation">
-                                    <option value="">00:00:00</option>
-                                    <option value="">01:00:00</option>
-                                    <option value="">02:00:00</option>
-                                    <option value="">03:00:00</option>
-                                    <option value="">04:00:00</option>
-                                    <option value="">05:00:00</option>
-                                    <option value="">06:00:00</option>
-                                    <option value="">07:00:00</option>
-                                    <option value="">08:00:00</option>
-                                    <option value="">09:00:00</option>
-                                    <option value="">10:00:00</option>
-                                    <option value="">11:00:00</option>
-                                    <option value="">12:00:00</option>
-                                    <option value="">13:00:00</option>
-                                    <option value="">14:00:00</option>
-                                    <option value="">15:00:00</option>
-                                    <option value="">16:00:00</option>
-                                    <option value="">17:00:00</option>
-                                    <option value="">18:00:00</option>
-                                    <option value="">19:00:00</option>
-                                    <option value="">20:00:00</option>
-                                    <option value="">21:00:00</option>
-                                    <option value="">22:00:00</option>
-                                    <option value="">23:00:00</option>
+                                <select name="start_time_operation" id="Line_start_time_operation">
+                                    <option value="00:00:00">00:00:00</option>
+                                    <option value="01:00:00">01:00:00</option>
+                                    <option value="02:00:00">02:00:00</option>
+                                    <option>03:00:00</option>   
+                                    <option>04:00:00</option>
+                                    <option >05:00:00</option>
+                                    <option >06:00:00</option>
+                                    <option  >07:00:00</option>
+                                    <option  >08:00:00</option>
+                                    <option  >09:00:00</option>
+                                    <option  >10:00:00</option>
+                                    <option  >11:00:00</option>
+                                    <option  >12:00:00</option>
+                                    <option  >13:00:00</option>
+                                    <option  >14:00:00</option>
+                                    <option  >15:00:00</option>
+                                    <option  >16:00:00</option>
+                                    <option  >17:00:00</option>
+                                    <option  >18:00:00</option>
+                                    <option  >19:00:00</option>
+                                    <option  >20:00:00</option>
+                                    <option  >21:00:00</option>
+                                    <option  >22:00:00</option>
+                                    <option  >23:00:00</option>
                                 </select>
 
                             </div>
 
                             <div class="row">
                                 <label for="Line_end_time_operation" class="required">End Time Operation <span class="required">*</span></label>        
-                                <select name="Line[end_time_operation]" id="end_time_operation">
-                                    <option value="">00:00:00</option>
-                                    <option value="">01:00:00</option>
-                                    <option value="">02:00:00</option>
-                                    <option value="">03:00:00</option>
-                                    <option value="">04:00:00</option>
-                                    <option value="">05:00:00</option>
-                                    <option value="">06:00:00</option>
-                                    <option value="">07:00:00</option>
-                                    <option value="">08:00:00</option>
-                                    <option value="">09:00:00</option>
-                                    <option value="">10:00:00</option>
-                                    <option value="">11:00:00</option>
-                                    <option value="">12:00:00</option>
-                                    <option value="">13:00:00</option>
-                                    <option value="">14:00:00</option>
-                                    <option value="">15:00:00</option>
-                                    <option value="">16:00:00</option>
-                                    <option value="">17:00:00</option>
-                                    <option value="">18:00:00</option>
-                                    <option value="">19:00:00</option>
-                                    <option value="">20:00:00</option>
-                                    <option value="">21:00:00</option>
-                                    <option value="">22:00:00</option>
-                                    <option value="">23:00:00</option>
+                                <select name="end_time_operation" id="Line_end_time_operation">
+                                    <option  >00:00:00</option>
+                                    <option  >01:00:00</option>
+                                    <option  >02:00:00</option>
+                                    <option  >03:00:00</option>
+                                    <option  >04:00:00</option>
+                                    <option  >05:00:00</option>
+                                    <option  >06:00:00</option>
+                                    <option  >07:00:00</option>
+                                    <option  >08:00:00</option>
+                                    <option  >09:00:00</option>
+                                    <option  >10:00:00</option>
+                                    <option  >11:00:00</option>
+                                    <option  >12:00:00</option>
+                                    <option  >13:00:00</option>
+                                    <option  >14:00:00</option>
+                                    <option  >15:00:00</option>
+                                    <option  >16:00:00</option>
+                                    <option  >17:00:00</option>
+                                    <option  >18:00:00</option>
+                                    <option  >19:00:00</option>
+                                    <option  >20:00:00</option>
+                                    <option  >21:00:00</option>
+                                    <option  >22:00:00</option>
+                                    <option  >23:00:00</option>
                                 </select>         
                             </div>
 
                             <div class="row">
-                                <label for="Line_type">Type</label>        <select name="Line[type]" id="Line_type">
+                                <label for="Line_type">Type</label>        <select name="type" id="Line_type">
                                     <option value="Tram">Tram</option>
                                     <option value="Bus">Bus</option>
                                     <option value="Nightliner">Nightliner</option>
@@ -97,33 +97,60 @@
 
                             <div class="row">
                                 <label for="Line_map" class="required">Map <span class="required">*</span></label>                       
-                                <input id="ytLine_map" type="hidden" value="" name="Line[map]"><input name="Line[map]" id="Line_map" type="file">            </div>
+                                <input name="map" id="Line_map" type="file">            </div>
 
                                         <div class="row buttons">
-                                            <input type="submit" name="yt0" value="Create">    </div>
+                                            <input type="submit" name="submit" value="Create">    </div>
 
                                         </form>
                                         </div><!-- form -->	</div><!-- content -->
 
-                                        <?php
+                        <?php
                             include 'db/connection.php'; 
+                            $all_codes = $conn->query("SELECT * FROM line");
+                            $errors = [];
+                            $validated =false;
 
                             if (isset($_POST['submit'])) {
-                                $name  = $_POST['name'];
-                                $capacity = $_POST['capacity'];
+                                $name = $_POST['name'];
+                                $start_time_operation = $_POST['start_time_operation'];
+                                $end_time_operation = $_POST['end_time_operation'];
                                 $type = $_POST['type'];
-                                $gos_number=$_POST['gos_number'];
-                                $line_id = $_POST['line_id'];
+                                $map = $_FILES['map'];
 
-                                $sql = "INSERT INTO vehicle (code) VALUES ('$name',  )";
-                                if ($conn->query($sql) == TRUE) {
-                                    echo "<p class='message'>Vehicle has been created successfully!</p>";
+                                $file_name = $_FILES['map']['name'];
+                                $file_size = $_FILES['map']['size'];
+                                $file_tmp = $_FILES['map']['tmp_name'];
+                                $file_type = $_FILES['map']['type'];
+                                $file_ext = strtolower(end(explode('.', $_FILES['map']['name'])));
+
+                                
+                                $sql = "INSERT INTO line (code, start_time_operation,end_time_operation, type, map) VALUES ('$name', '$start_time_operation', '$end_time_operation',  '$type', '$file_name')";
+
+                                // $extensions = ['jpeg', 'png', 'jpg', 'gif'];
+
+                                // if (in_array($file_ext, $extensions)) {
+                                //     $errors[] = "i need 'jpeg', 'png', 'jpg' or 'gif' format";
+                                // }
+
+                                if ($file_size  > 10097152) {
+                                    $errors[] =  'File size must be less than 10mb';
+                                }
+
+                                if (empty($errors) == true) {
+                                    move_uploaded_file($file_tmp , "images/" . $file_name);
+                                    // echo 'Uploaded';
                                 } else {
-                                    echo "Error: " . $sql . "<br>" . $conn->error;
+                                    print_r($errors);
+                                }
+
+                                if ($conn->query($sql) == TRUE) {
+                                    echo "<p class='message'>Line has been created successfully!</p>";
+                                    
+                                } else {
+                                    echo "Error: ".$sql.'<br>'.$conn->error;
                                 }
                             }
-
-                            $conn->close();
                         ?>
 
 
